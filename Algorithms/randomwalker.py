f9167727d1f3cefdc5aec78b03523900e228e067
Esteban Castillo <escaob1@gmail.com>
2020-03-04 00:12:07 +0100

    Good debugging on OBTAINING THE WAY

--- Algorithms/randomwalker.py
+++ Algorithms/randomwalker.py
@@ -1,17 +1,46 @@
 import sys
 import random
 
-#Se definen las coordenadas del plano en el que se mueve nuestro caminante
+# WORLD -------> (WIDTH, HEIGTH) = (X, Y)
 x = int(sys.argv[1])
 y = int(sys.argv[2])
-coor = [x,y]
+origin = [x,y]
 
+print "----      ORIGIN\n" + str(origin) + "\n----"
+def action(origin):
+	origincopy = origin[:]
+	theway = []
+	theway.append({":x" : origincopy[0], ":y" : origincopy[1]})
+	
+	print range(int(sys.argv[3]))
+	print "ENTERING LOOP"	
+	for pace in range(int(sys.argv[3])):	# PACEs number
+		print "******" + str(pace) + "*******"
+		for axis in range(len(origincopy)):			#(x, y, z, ...)	--------> (x + k, y, z) / (x , y + k, z) /  (x , y, z + k)
+			
+			change = random.randint(-1,1)			#DIRECTION	--------> +-(k, K)
+			origincopy[axis] = origincopy[axis] + change	#TRANSFORMATION
+			print "......\n" + str(origincopy) + "\n......"
+		theway.append({":x" : origincopy[0], ":y" : origincopy[1]})	
+		print "FINAL -------> " + str(origincopy)
+	print "OUT OF LOOP"
+	finalpos = origincopy
+	return finalpos, theway
+action(origin)
 
-for x in range(0,int(sys.argv[3])):	#La cantidad de pasos que va a dar
-	#print("---")
-	for i in range(0,len(coor)):	#Cada paso es aleatorio entre 3 opciones
-		value = random.randint(-1,1)
-		#print("Para " + str(coor[i]) + "la variacion es " + str(value))
-		coor[i] = coor[i] + value
+#FUNCTION FRAMEWORK
+def present(x):
+	print str(x)
 
-print(coor) #coordenadas finales
+def renderpos(x):
+	print "-"
+
+def renderwalker():
+	print "X"
+
+#map(lambda x: present(x[":x"]), action(origin)[1])
+
+#renderwalker()
+
+#To create places {":reihe": K, ":column": K} -----> coor = [reihe, column]
+#map(lambda reihe: map(lambda column: map(lambda pair: present(pair),[{":reihe" : reihe, ":column" : column}]), range(coor[1])), range(coor[0]))
