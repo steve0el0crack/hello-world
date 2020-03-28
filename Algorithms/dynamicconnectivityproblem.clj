16ef69c5c8fdfb993c6fe31af164a311a1dde59c
Esteban Castillo <escaob1@gmail.com>
2020-03-29 00:12:40 +0100

    The atoms (alone) CANNOT know the total of their network, but their direct-connections

--- Algorithms/dynamicconnectivityproblem.clj
+++ Algorithms/dynamicconnectivityproblem.clj
@@ -8,19 +8,30 @@
     true
     false))
 
-;;at the beginning all the elements in the universe must be unconnected, this may chane and therefore the correct structure is an atom. The difference between them will be their positions in the universe (index's)
-(def universe (apply vector (map (fn [index] (atom #{index})) (range number-of-elements))))
+;;at the beginning all the elements in the universe must be unconnected, this may chane and therefore the correct structure is an atom. The difference between them will be the key of the dictionary... the starting index in the universe of the atom.
+(def universe (apply vector (map (fn [index] (atom {index #{index}})) (range number-of-elements))))
+
+
 
 ;;The connection is made in pairs. The question is how do we retain and represent that binding... in the atoms themselves
 (defn connect
   [x y]
   (let [origin (nth universe x)
-        goal (nth universe y)
+        destiny (nth universe y)
         i x
         f y]
-    (map (fn [pair] (swap! (first pair) (fn [inmediate-neighbours] (conj inmediate-neighbours (second pair)))))
-         [[origin f] [goal x]])
-    )) 
+    (map (fn [data]
+           (let [atom (first data)
+                 key  (second data)
+                 new-val (nth data 2)]
+             (swap! atom
+                    (fn [atom-value]
+                      (update atom-value key
+                              (fn [path]
+                                (conj path new-val)))))))
+         [[origin i f]
+          [destiny f i]])))
+
 ;;but the connections have 2 conditions: An element cannot connect to itself, and a the operation to connect a pair should not be repeated for the same pair.
 (defn generate-pair []
   (loop [output [(rand-int number-of-elements) (rand-int number-of-elements)]]
@@ -29,9 +40,18 @@
       output)))
 
 ;;now we can start binding our elements in the universe and seeing what happens!
-(map (fn [_] (connect (first (generate-pair)) (second (generate-pair))))
+(map (fn [_]
+       (let [pair (generate-pair)
+             origin (first pair)
+             goal (second pair)]
+         (connect origin goal)))
      (range number-of-bindings))
 
-universe
+;;leting aside the fact that a binding can be applied to the same pair of atoms, we continue defining the other core ideas of the algorithm and problem
+(defn get-direct-connections []
+  (filter (fn [path] (> (count path) 1))
+          (apply vector (map (fn [atom] (first (apply vector (vals @atom)))) universe))))
+(get-direct-connections)
+
 
 
